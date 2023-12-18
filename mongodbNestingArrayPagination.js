1629d3010a4479616f27213e2bbfb2a575c30d1f
Saimon Pranta <101664170+SaimonPranta@users.noreply.github.com>
2023-12-18 10:28:46 +0600

    Update mongodbNestingArrayPagination.js

--- mongodbNestingArrayPagination.js
+++ mongodbNestingArrayPagination.js
@@ -32,4 +32,121 @@ User.findOne({ '_id': ObjectId(userId) })
 
 
 
+// {
+//   "_id": {
+//     "$oid": "650205290152c6d3eb546b09"
+//   },
+//   "username": "symul_12345",
+//   "country": "BD",
+//   "reportType": "campaign",
+//   "reports": [
+//     {
+//       "dayInfo": "13-12-2023",
+//       "activities": [
+//         {
+//           "offerID": "3139",
+//           "name": "Credit Score Hero ($1 Trial)",
+//           "activitiesType": "click",
+//           "clicks": 2,
+//           "leads": 0,
+//           "reversal": 0,
+//           "reversalAmount": 0,
+//           "incomes": 0,
+//           "status": "active",
+//           "_id": {
+//             "$oid": "65798ab51fcc9f6568273b6e"
+//           }
+//         },
+//         {
+//           "offerID": "19030",
+//           "name": "Mobile Xpression (iOS)",
+//           "activitiesType": "click",
+//           "clicks": 1,
+//           "leads": 0,
+//           "reversal": 0,
+//           "reversalAmount": 0,
+//           "incomes": 0,
+//           "status": "active",
+//           "_id": {
+//             "$oid": "65798a7c1fcc9f6568273aab"
+//           }
+//         }
+//       ],
+//       "_id": {
+//         "$oid": "65798a7c1fcc9f6568273aaa"
+//       }
+//     },
+//     {
+//       "dayInfo": "11-12-2023",
+//       "activities": [
+//         {
+//           "offerID": "24669",
+//           "name": "PaidResearchSurvey",
+//           "activitiesType": "click",
+//           "clicks": 1,
+//           "leads": 0,
+//           "reversal": 0,
+//           "reversalAmount": 0,
+//           "incomes": 0,
+//           "status": "active",
+//           "_id": {
+//             "$oid": "6577c57ac765a7771dc422db"
+//           }
+//         },
+//         {
+//           "offerID": "3139",
+//           "name": "Credit Score Hero ($1 Trial)",
+//           "activitiesType": "click",
+//           "clicks": 1,
+//           "leads": 0,
+//           "reversal": 0,
+//           "reversalAmount": 0,
+//           "incomes": 0,
+//           "status": "active",
+//           "_id": {
+//             "$oid": "6577c55cc765a7771dc4214a"
+//           }
+//         }
+//       ],
+//       "_id": {
+//         "$oid": "6577c55cc765a7771dc42149"
+//       }
+//     },
+//     {
+//       "dayInfo": "06-12-2023",
+//       "activities": [
+//         {
+//           "offerID": "23221",
+//           "name": "ConsumerTestConnect DoorDash",
+//           "activitiesType": "click",
+//           "clicks": 1,
+//           "leads": 0,
+//           "reversal": 0,
+//           "reversalAmount": 0,
+//           "incomes": 0,
+//           "status": "active",
+//           "_id": {
+//             "$oid": "65704fd546f4c304067932af"
+//           }
+//         }
+//       ],
+//       "_id": {
+//         "$oid": "65704fd546f4c304067932ae"
+//       }
+//     },
+//       ],
+//       "_id": {
+//         "$oid": "650205290152c6d3eb546b0a"
+//       }
+//     }
+//   ],
+//   "createdAt": {
+//     "$date": "2023-09-13T18:53:29.212Z"
+//   },
+//   "updatedAt": {
+//     "$date": "2023-12-14T01:48:45.811Z"
+//   },
+//   "__v": 0,
+//   "disableList": []
+// }
 
